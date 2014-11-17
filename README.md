13f2c6e54bf3ffc631dd2a8611d5fb9fec278703
Jukka Paasonen <paazmaya@yahoo.com>
2014-11-17 17:58:32 +0200

    Logo and initial wondering

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# gulp-sakugawa
+# gulp-sakugawa (グルプ佐久川)
 
 > Run [Sakugawa](https://github.com/paazmaya/sakugawa "CSS splitter, filter and organiser")
 > via [gulp](http://gulpjs.com/ "The streaming build system"), for CSS splitting, filtering and organising.
 
+![Mr Sakugawa enjoying the taste of gulpt](./gulp-sakugawa-logo.png)
 [![Analytics](https://ga-beacon.appspot.com/UA-2643697-15/gulp-sakugawa/index)](https://github.com/igrigorik/ga-beacon)
 
 ## Version history
---
+++ index.js
@@ -0,0 +1,49 @@
+// through2 is a thin wrapper around node transform streams
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+var sakugawa = require('sakugawa');
+
+// consts
+const PLUGIN_NAME = 'gulp-sakugawa';
+
+function prefixStream(prefixText) {
+  var stream = through();
+  stream.write(prefixText);
+  return stream;
+}
+
+// plugin level function (dealing with files)
+function gulpSakugawa(prefixText) {
+  if (!prefixText) {
+    throw new PluginError(PLUGIN_NAME, 'Missing prefix text!');
+  }
+
+  prefixText = new Buffer(prefixText); // allocate ahead of time
+
+  // creating a stream through which each file will pass
+  var stream = through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      // do nothing if no contents
+    }
+
+    if (file.isBuffer()) {
+      file.contents = Buffer.concat([prefixText, file.contents]);
+    }
+
+    if (file.isStream()) {
+      file.contents = file.contents.pipe(prefixStream(prefixText));
+    }
+
+    this.push(file);
+
+    return cb();
+  });
+
+  // returning the file stream
+  return stream;
+};
+
+// exporting the plugin main function
+module.exports = gulpSakugawa;
